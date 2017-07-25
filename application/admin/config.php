a2651af45f1aa93ff70bff1c135474e30a9236a4
7howe <623640221@qq.com>
2017-07-25 18:07:04 +0800

    添加产品新增、推荐、删除、修改功能

--- application/admin/config.php
+++ application/admin/config.php
@@ -1,2 +1,10 @@
 <?php
 
+return [
+    'uploads' => [
+        'path' => ROOT_PATH . 'public' . DS . 'data' . DS . 'UploadFiles',
+        'product_path' => ROOT_PATH . 'public' . DS . 'data' . DS . 'UploadFiles' . DS . 'product',
+        'brand_path' => ROOT_PATH . 'public' . DS . 'data' . DS . 'UploadFiles' . DS .'brand',
+        'category' => ROOT_PATH . 'public' . DS . 'data' . DS . 'UploadFiles' . DS .'category'
+    ]
+];
--- application/admin/controller/Product.php
+++ application/admin/controller/Product.php
@@ -11,6 +11,15 @@ use think\Controller;
 
 class Product extends Controller
 {
+    protected $upload_path;
+    protected $upload_show;
+
+    protected function _initialize()
+    {
+        parent::_initialize();
+        $this->upload_path = config('uploads.product_path');
+        $this->upload_show = 'UploadFiles' . DS . 'product';
+    }
     public function index()
     {
         $map['pro_type'] = 1;
@@ -41,14 +50,120 @@ class Product extends Controller
         $this->assign('img_str',null);
 
         if ($this->request->isPost()) {
-
+            $array=array(
+                'name'=>$_POST['name'] ,
+                'intro'=>$_POST['intro'] ,
+                'shop_id'=>  0 ,//所属店铺
+                'cid'=> intval($_POST['cid']) ,			//产品分类ID
+                'brand_id'=> intval($_POST['brand_id']) ,//产品品牌ID
+                'pro_number'=>$_POST['pro_number'] ,	//产品编号
+                'sort'=>0 ,
+                'price'=>(float)$_POST['price'] ,
+                'price_yh'=>(float)$_POST['price_yh'] ,
+                'price_jf'=>(float)$_POST['price_jf'] ,//赠送积分
+                'updatetime'=>time(),
+                'num'=>(int)$_POST['num'] ,			//库存
+                'content'=>$_POST['content'] ,
+                'company'=>$_POST['company'],  //产品单位
+                'pro_type'=>1,
+                'renqi' => intval($_POST['renqi']),
+                'is_hot'=>intval($_POST['is_hot']),//是否热卖
+                'is_show'=>intval($_POST['is_show']),//是否新品
+                'is_sale'=>0,//是否折扣
+            );
+            if ($photo_x = $this->request->file('photo_x')) {
+                $info = $photo_x->move($this->upload_path);
+                $info && $array['photo_x'] = $this->upload_show .DS.$info->getSaveName();
+            }
+            if ($photo_d = $this->request->file('photo_d')) {
+                $info = $photo_d->move($this->upload_path);
+                $info && $array['photo_d'] = $this->upload_show .DS.$info->getSaveName();
+            }
+            $photo_string = '';
+            foreach ($this->request->file('files') as $pic) {
+                $info = $pic->move($this->upload_path);
+                $info && $photo_string .= $this->upload_show .DS.$info->getSaveName() .',';
+            }
+            $photo_string != '' && $array['photo_string']=trim($photo_string,',');
+            $res = db('product')->data($array)->insert();
+            if ($res) {
+                $this->success('添加产品成功');
+            } else {
+                $this->error('添加产品失败');
+            }
         } else {
-            $cate = db('category')->field('id,tid,name')->select();
-
+            $cate = db('category')->field('id,tid,name')->where('tid','eq',1)->select();
             $brand = db('brand')->field('id,name')->select();
             $this->assign('cate_list',$cate);
             $this->assign('brand_list',$brand);
             return $this->fetch();
+        }
+
+    }
+
+    public function getcid()
+    {
+        $cid = $this->request->post('cateid') ? : null;
+        if (intval($cid)) {
+            $list = db('category')->where('tid','eq',intval($cid))->select();
+            $data['catelist'] = $list;
+            return $data;
+        } else {
+            $this->error('未指定父类ID');
+        }
+    }
+
+    public function set_tj()
+    {
+        if ($pro_id = $this->request->get('pro_id')) {
+            $map['id'] = intval($pro_id);
+            $pro_info = db('product')->field('type')->find($pro_id);
+            if(!$pro_info){
+                $this->error('非法产品id');
+            }else{
+                $data['type'] = $pro_info['type'] === 1 ? 0 : 1;
+                $info = db('product')->where($map)->update($data);
+                if ($info) {
+                    $msg = $data['type'] ? '推荐成功':'取消推荐';
+                    $this->success($msg);
+                } else{
+                    $this->error('操作失败');
+                }
+            }
+        }
+
+    }
+
+    public function del()
+    {
+        if ($did = $this->request->get('did')) {
+            $pro_info = db('product')->find(intval($did));
+            if ($pro_info) {
+                $map['id'] = intval($did);
+                $data['del'] = $pro_info['del'] === 1 ? 0 : 1;
+                $res = db('product')->where($map)->update($data);
+                if ($res) {
+                    $msg = $data['del'] ? '删除成功': '恢复成功';
+                    $this->success($msg);
+                } else {
+                    $this->error('操作失败');
+                }
+            } else{
+                $this->error('非法产品ID');
+            }
+        }
+    }
+
+    public function edit()
+    {
+        if ($pid = $this->request->get('pid')) {
+            $pro_info = db('product')->find($pid);
+            if ($pro_info) {
+                $this->assign('name',$pro_info['name']);
+                return $this->fetch('add');
+            } else {
+                $this->error('非法产品ID');
+            }
         }
     }
 }
