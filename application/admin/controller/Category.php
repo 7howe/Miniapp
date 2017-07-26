25a31409a965c4e3b810510873a13bfa1ecc11d3
7howe <623640221@qq.com>
2017-07-26 17:53:21 +0800

    完善后台新增、编辑、推荐、删除等功能

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -13,8 +13,13 @@ use think\Controller;
 
 class Category extends Controller
 {
+    protected $category_path;
+    protected $category_show = 'UploadFiles'.DS.'category';
+
     public function _initialize()
     {
+        parent::_initialize();
+        $this->category_path  = config('uploads.category_path');
         $list = db('category')->field('id,tid,name,bz_2,bz_4')->select();
         foreach($list as $k=>$v){
             if($v['tid']===0){
@@ -38,11 +43,41 @@ class Category extends Controller
 
     public function add()
     {
+        echo $this->category_path;
         if ($this->request->isPost()) {
-
+            $data = $this->request->post();
+            if ($thumb = $this->request->file('file')) {
+                $data['bz_1'] = $this->category_show .DS. $thumb->move($this->category_path)->getSaveName();
+            }
+            $info = db('category')->insert($data);
+            $info ? $this->success('添加成功') : $this->error('操作失败');
         } else {
             $this->assign('cate_info',null);
             return $this->fetch();
         }
+    }
+
+    public function edit()
+    {
+        if ($this->request->isPost()) {
+
+        } else {
+            $id = intval($this->request->get('cid'));
+            $cateinfo = db('category')->find($id);
+            $this->assign('cate_info',$cateinfo);
+            return $this->fetch('add');
+        }
+    }
+
+    public function set_tj()
+    {
+
+    }
+
+    public function del($did)
+    {
+        $did = intval($did);
+        $info = db('category')->where('id', 'eq', $did)->delete();
+        $info ? $this->success('删除成功') : $this->error('未知错误');
     }
 }
--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -20,4 +20,17 @@ class User  extends Controller
         return $this->fetch();
 
     }
+
+    public function del($did)
+    {
+        $did = intval($did);
+        if ($user = db('user')->find($did)) {
+            $data['del'] =$user['del'] == 1 ? 0 :1;
+            $msg = $data['del'] ? '禁用成功' : '恢复成功';
+            $info = db('user')->where('id','eq',$did)->update($data);
+            $info ? $this->success($msg) : $this->error('操作失败');
+        } else {
+            $this->error('非法ID');
+        }
+    }
 }
